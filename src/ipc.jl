584c745bb27eb1d9c990f946ef749c210dc10136
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2025-05-15 17:36:02 -0600

    add comments and change offset to 8

--- src/ipc.jl
+++ src/ipc.jl
@@ -1,5 +1,7 @@
 # high level global definitions for plug-and-play
 
+# for now the declaration of a variable only stores the offset to the first byte, the type
+# and the size $(n, m)$ (might be (0,0) for scalar, (n,0) for vector, or (n,m) for matrix)
 struct VarDecl
     offset::Int
     type::Int
@@ -7,6 +9,9 @@ struct VarDecl
     m::Int
 end
 
+# the IPC struct stores the memory map, the semaphore to signal from Julia to FreeFem and
+# from FreeFem to Julia, the size of the memory map and the pairs of names to variable
+# declarations
 struct IPC
     size::Int
     mmap::Mmap
@@ -18,18 +23,39 @@ end
 IPC(nmmap, memory, nsemjl, nsemff) =
     IPC(0, Mmap(nmmap, memory), Sem(nsemjl), Sem(nsemff), Dict{String,VarDecl}())
 
+# In the next section is defined the IPC protocol for exchanging memory objects supported by
+# the `mmap-semaphore-sv` plugin for FreeFem. Currently only the next types are supported
+# for sending data
+#
+#   - int               ->  Int64
+#   - real              ->  Float64
+#   - complex           ->  ComplexF64
+#   - int[int]          ->  Vector{Int64}
+#   - real[int]         ->  Vector{Float64}
+#   - complex[int]      ->  Vector{ComplexF64}
+#   - int[int,int]      ->  Matrix{Int64}
+#   - real[int,int]     ->  Matrix{Float64}
+#   - complex[int,int]  ->  Matrix{ComplexF64}
+#
+# It is possible to send a string, but that is only used to syncronize the names of the
+# variables. Tha ABI for sending strings (string => String) is [size|data].
+
+# this routine checks if the variable declared is a vector, a matrix or a scalar
+# ang returns the size of the variable in bytes ()
 function varsize(vard::VarDecl)
+    # type 1 (001) is Int, 2 (010) is Float64, and 4 (100) is ComplexF64.
     typesize = (vard.type == 1 || vard.type == 2) ? 8 : (vard.type == 4 ? 16 : -1)
     isvec = (vard.n != 0)
     ismat = (vard.n != 0 && vard.m != 0)
     typesize * (ismat ? vard.n * vard.m : (isvec ? vard.n : 1))
 end
 
+# the protocol to register vars is 
 function register_vars(ipc::IPC)
     wait(ipc.semff)
     declsize = read(ipc.mmap, Int, 0)
-    decloffset = 128
-    offset = 128
+    decloffset = 8
+    offset = 8
     vars = Dict{String,VarDecl}()
     while decloffset < declsize
         vard = read(ipc.mmap, VarDecl, decloffset)::VarDecl
